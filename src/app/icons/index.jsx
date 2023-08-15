2337cd692bec59764a8a63b64096e1a9566700d8
nixon <jeromenixon95@gmail.com>
2023-08-15 15:15:15 -0700

    capitalized L

--- src/app/icons/index.jsx
+++ src/app/icons/index.jsx
@@ -10,6 +10,6 @@ export { default as UpArrow } from './UpArrow';
 export { default as VolumeIcon } from './VolumeIcon';
 export { default as YouTubeShuffle } from './YouTubeShuffle';
 export { default as GitHub } from './GitHub';
-export { default as LinkedIn } from './linkedIn';
+export { default as LinkedIn } from './LinkedIn';
 export { default as Verified } from './Verified';
 export { default as SearchIcon } from './SearchIcon';
